aab36eca7c52546c3df563daf7ac943213ae9f9c
Agustin <pacg93@hotmail.com>
2023-03-07 14:31:33 -0300

    Creo la pagina de ejemplo y subo la imagen de prueba

--- src/modulos/paginaejemplo.vue
+++ src/modulos/paginaejemplo.vue
@@ -1,4 +1,22 @@
-<template><h1>Pagina de Ejemplo</h1></template>
+<template>
+  <div>
+    <h1>Pagina de Ejemplo</h1>
+    <v-row>
+      <v-col cols="4">
+        <v-img
+          src="E:\Proyectos\Repositorio Compartido\VUE\ventaEntradas\src\assets\ratablanca-sde.jpg"
+          alt="Example image"
+          width="400"
+          height="300"
+          :lazy-src="true"
+          aspect-ratio="1.33"
+          placeholder="https://example.com/placeholder.jpg"
+        />
+      </v-col>
+      <v-col cols="8"> </v-col>
+    </v-row>
+  </div>
+</template>
 
 <script>
 export default {
